862f7e5789aa90bdb256b9c14bfbed12201dc46d
Andrew Berezin <berezinandriy@gmail.com>
2022-09-20 09:47:38 -0400

    Add bybit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { createStyles, Title, Text, Button, Container, Group, Space, Table, ScrollArea, MultiSelect, Checkbox, Tabs } from '@mantine/core';
 import './App.css'
+import bybit from './bybit.json' 
 
 const useStyles = createStyles((theme) => ({
   redText: {
@@ -16,19 +17,23 @@ function App() {
   const [longRates, setLongRates] = useState();
   const [shortRates, setShortRates] = useState();
   const [topRates, setTopRates] = useState();
+  const [bybitRates, setBybitRates] = useState();
   const [nonTradableCoins, setNonTradableCoins] = useState();
   const [selectedNonTradableCoins, setSelectedNonTradableCoins] = useState(
     (localStorage.getItem('coins') && JSON.parse(localStorage.getItem('coins')))  || []
   );
 
+  //QueryString
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const bybitParam = urlParams.get('bybit')
+
   // data.data.cryptoCurrencyList.map(c=>c.symbol+"-PERP")
   const topCoins = ['BTC-PERP', 'ETH-PERP', 'USDT-PERP', 'USDC-PERP', 'BNB-PERP', 'BUSD-PERP', 'XRP-PERP', 'ADA-PERP', 'SOL-PERP', 'DOGE-PERP', 'DOT-PERP', 'DAI-PERP', 'MATIC-PERP', 'SHIB-PERP', 'TRX-PERP', 'AVAX-PERP', 'WBTC-PERP', 'LEO-PERP', 'UNI-PERP', 'ETC-PERP', 'LTC-PERP', 'FTT-PERP', 'LINK-PERP', 'ATOM-PERP', 'CRO-PERP', 'NEAR-PERP', 'XLM-PERP', 'XMR-PERP', 'BCH-PERP', 'ALGO-PERP', 'FLOW-PERP', 'VET-PERP', 'ICP-PERP', 'FIL-PERP', 'EOS-PERP', 'APE-PERP', 'MANA-PERP', 'SAND-PERP', 'HBAR-PERP', 'XTZ-PERP', 'CHZ-PERP', 'EGLD-PERP', 'QNT-PERP', 'AXS-PERP', 'AAVE-PERP', 'THETA-PERP', 'TUSD-PERP', 'BSV-PERP', 'OKB-PERP', 'USDP-PERP', 'ZEC-PERP', 'KCS-PERP', 'BTT-PERP', 'MIOTA-PERP', 'HT-PERP', 'MKR-PERP', 'HNT-PERP', 'USDD-PERP', 'KLAY-PERP', 'GRT-PERP', 'XEC-PERP', 'FTM-PERP', 'USDN-PERP', 'SNX-PERP', 'RUNE-PERP', 'NEO-PERP', 'PAXG-PERP', 'CRV-PERP', 'CAKE-PERP', 'NEXO-PERP', 'LDO-PERP', 'BAT-PERP', 'WAVES-PERP', 'DASH-PERP', 'ZIL-PERP', 'STX-PERP', 'LRC-PERP', 'ENJ-PERP', 'MINA-PERP', 'FEI-PERP', 'KAVA-PERP', 'GMT-PERP', 'BTG-PERP', 'XEM-PERP', 'DCR-PERP', 'TWT-PERP', 'GNO-PERP', 'KSM-PERP', '1INCH-PERP', 'CELO-PERP', 'AR-PERP', 'HOT-PERP', 'GALA-PERP', 'ANKR-PERP', 'XDC-PERP', 'CVX-PERP', 'GT-PERP', 'COMP-PERP', 'TFUEL-PERP', 'QTUM-PERP']
-  
   const { classes } = useStyles(); 
 
   useEffect(() => {
     getFundingRates();
-    //getKuCoinSymbols()
   }, []);
 
   const getFundingRates = () => {
@@ -41,15 +46,6 @@ function App() {
       });
   };
 
-  const getKuCoinSymbols = () => {
-    fetch("https://api-futures.kucoin.com/api/v1/contracts/active")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-      }); 
-  }
 
   const splitFundingRates = ({result}) => {
     
@@ -78,26 +74,43 @@ function App() {
     const shortRates = finalCoins.filter(i=>i.rate >= 0).sort((a,b) =>b.rate-a.rate)
     const longRates = finalCoins.filter(i=>i.rate < 0).sort((a,b) =>a.rate-b.rate)
     const topRates = finalCoins.filter(i=>topCoins.includes(i.future)).sort((a,b) =>Math.abs(b.rate)-Math.abs(a.rate))
+    const bybitSymbols = bybit.data.marketPairs.map(i=>i.baseSymbol)
+    const bybitRates = topRates.filter(i=>bybitSymbols.includes(i.future.replace("-PERP", "")))
     setFundingRates(finalCoins)
     setLongRates(longRates);
     setShortRates(shortRates);
     setTopRates(topRates)
+    setBybitRates(bybitRates)
   };
 
-  const showRows = (rates, top = false) => rates.map((element, index) => (
-    <tr key={element.future}>
-      <td style={{width:'10px'}}><Text>{index+1}</Text></td>
-      <td>
-        <Checkbox/>
-      </td>
-      <td>{element.future}</td>
-      <td 
-       className={ top ? element.rate>0 ? classes.redText : classes.greenText : ''}
-      >{element.rate}</td>
-      <td><a href={"https://www.tradingview.com/chart?symbol=FTX:"+ element.future.replace('-','')} 
-      target="_blank">Chart</a></td>
-    </tr>
-  ));
+  const showRows = (rates, top = false, exchange="FTX") => rates.map((element, index) => {
+    let future, futureUrlSymbol
+    switch (exchange) {
+      case "FTX":
+        future = element.future
+        futureUrlSymbol = element.future.replace('-','')
+        break;
+      case "BYBIT":
+        future = element.future.replace("-PERP", "/USDT")
+        futureUrlSymbol = element.future.replace('-PERP','USDT')
+        break; 
+    }
+
+    return (
+      <tr key={element.future}>
+        <td style={{width:'10px'}}><Text>{index+1}</Text></td>
+        <td>
+          <Checkbox/>
+        </td>
+        <td>{future}</td>
+        <td 
+          className={ top ? element.rate>0 ? classes.redText : classes.greenText : ''}
+        >{element.rate}</td>
+        <td>
+          <a href={`https://www.tradingview.com/chart?symbol=${exchange}:${futureUrlSymbol}`} 
+          target="_blank">Chart</a></td>
+      </tr>
+  )});
 
   const addNonTradabelCoins = (coins) => {
     localStorage.setItem('coins', JSON.stringify(coins));
@@ -149,6 +162,29 @@ function App() {
               </ScrollArea>
             </Container>
         </Tabs.Tab>
+        { bybitParam && 
+        <Tabs.Tab label="Top 100 (Bybit)">
+            <Container p={0} mr="xl">
+              <ScrollArea  sx={(theme) => ({
+                border: `1px solid ${theme.colors.gray[3]}`,
+                borderRadius: 5
+                })}>
+              { bybitRates && 
+              ( <Table striped highlightOnHover>
+                  <thead>
+                    <tr>
+                      <th style={{width:'10px'}}></th>
+                      <th></th>
+                      <th>Future</th>
+                      <th>Rate</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>{showRows(bybitRates, true, "BYBIT")}</tbody>
+                </Table>)}
+              </ScrollArea>
+            </Container>
+        </Tabs.Tab>}
         <Tabs.Tab label="All Coins">
         <Group spacing="xl" grow align="top">
             <Container p={0} mr="xl">
